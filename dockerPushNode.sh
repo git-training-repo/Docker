43e06427ff3da0020e724cb83c0fb69cbdb642f1
git-training-repo <41342781+git-training-repo@users.noreply.github.com>
2018-09-02 21:29:03 +0530

    updated image name

--- dockerPushNode.sh
+++ dockerPushNode.sh
@@ -1,11 +1,11 @@
 #Pushing to docker hub
 #specify login credentials here(1)
 docker login -u="veboddu" -p="Chinnu@30"
-#image name must be iiq
-imgid=$(docker images -q iiq)
+#image name must be iiqpuppet 
+imgid=$(docker images -q iiqpuppet)
 echo $imgid
 #specify login credentials here(2)
-docker tag $imgid veboddu/beatles:iiq
+docker tag $imgid veboddu/beatles:iiqpuppet
 #specify login credentials here(3)
 docker push veboddu/beatles
 echo "Pushed to docker hub"
--- gitPull.sh
+++ gitPull.sh
@@ -1,17 +1,20 @@
-cd /root
-mkdir tempdir
-cd tempdir
-#cleaning Repo
-sshpass -p "Qwerty12" ssh root@192.168.56.4 'bash /opt/DEVOPS/Docker/cleanRepoNode.sh'
-echo "Docker Push Job done"
+#cd /root
+#mkdir tempdir
+#cd tempdir
 yum -y install sshpass
-git clone https://github.com/git-training-repo/Docker.git
+#git clone https://github.com/git-training-repo/Docker.git
 cd Docker
+
+#cleaning Repo
+sshpass -p "Qwerty12" scp cleanRepoNode.sh root@192.168.56.4:/opt/DEVOPS/Docker
+sshpass -p "Qwerty12" ssh root@192.168.56.4 'bash /opt/DEVOPS/Docker/cleanRepoNode.sh'
+
 for entry in "$PWD"/*
 do
   echo "$entry"
   sshpass -p "Qwerty12" scp $entry root@192.168.56.4:/opt/DEVOPS/Docker
 done
+
 cd /opt/DEVOPS/Demo/Dev_War
 sshpass -p "Qwerty12" scp identityiq.war root@192.168.56.4:/opt/DEVOPS/Docker
 echo "Placed Files on Node, Job done"
